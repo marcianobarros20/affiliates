cb7ecbbeffa5aa1ece52b09aba6b5a32e7a9f7cc
Gargitier5 <iamgargi92@gmail.com>
2016-03-28 18:37:13 +0530

    chenged map

--- application/views/contact_us.php
+++ application/views/contact_us.php
@@ -34,7 +34,7 @@
     <!-- /header -->
 
     <section class="no-margin">
-        <iframe width="100%" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com.au/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=Dhaka,+Dhaka+Division,+Bangladesh&amp;aq=0&amp;oq=dhaka+ban&amp;sll=40.714353,-74.005973&amp;sspn=0.836898,1.815491&amp;ie=UTF8&amp;hq=&amp;hnear=Dhaka+Division,+Bangladesh&amp;t=m&amp;ll=24.542126,90.293884&amp;spn=0.124922,0.411301&amp;z=8&amp;output=embed"></iframe>
+        <iframe width="100%" height="200" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d49799.26337019521!2d-85.69035624773345!3d38.73034377843344!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x886bd20ec02fdea7%3A0x1c16995e39978d1!2sTier5!5e0!3m2!1sen!2s!4v1458908236414"></iframe>
     </section>
 
     <section id="contact-page" class="container">
