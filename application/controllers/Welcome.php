19ead9e0e0b0de71065eb43faa39ed17773747aa
sudiptamitra <sudiptamit1@gmail.com>
2016-03-30 18:36:51 +0530

    email changed

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -322,8 +322,8 @@ class Welcome extends CI_Controller {
             $msg=$name."<br>".$this->input->post('message');
             
             $this->email->from($email);
-	        $this->email->to('hello@tier5.us');
-	        $this->email->cc('sudiptamit1@gmail.com');  
+	        $this->email->to('sudiptamit1@gmail.com');
+	        //$this->email->cc('sudiptamit1@gmail.com');  
 	        
 	        $this->email->subject('Customer Query');
 			$this->email->message($msg);
