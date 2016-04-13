69e48ff79343210f320f54b265c49b8fee17342c
sudiptamitra <sudiptamit1@gmail.com>
2016-04-13 16:09:03 +0530

    made changes in header

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -163,7 +163,7 @@
                 </div>
                 <div class="span6">
                     <ul class="breadcrumb pull-right">
-                        <li><a href="<?php echo base_url();?>">Home</a> <span class="divider"></span></li>
+                        <li><a href="<?php echo base_url();?>">Home</a></li>
                         <li class="active">Dashboard</li>
                     </ul>
                 </div>
