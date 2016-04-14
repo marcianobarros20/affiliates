6ffa41b63f5131129f74156e0bbfc1770e6545b9
sudiptamitra <sudiptamit1@gmail.com>
2016-04-14 12:08:37 +0530

    made changes in admin

--- application/views/admin/add_course.php
+++ application/views/admin/add_course.php
@@ -38,6 +38,8 @@
         <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
     <script type="text/javascript" src="js/affiliate.js"></script>
   </head>
   <body class="hold-transition skin-blue sidebar-mini">
