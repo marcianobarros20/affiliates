c08dcc014563d726e275bfd8f4ade82c5ddd65bf
sudiptamitra <sudiptamit1@gmail.com>
2016-04-14 12:59:18 +0530

    made changes in frame.js file

--- js/frame.js
+++ js/frame.js
@@ -4,3 +4,25 @@
        */
  $('.fancybox').fancybox();
 });
+
+  $(document).delegate('#list a','click',function(){
+ //alert($(this).data("id"));
+ var parent=$(this).data("id");
+if($('.par_'+parent).hasClass("display_block"))
+{
+$('.par_'+parent).removeClass('display_block');
+$('.par_'+parent).addClass('new_display2');
+$('.param_'+parent).html(' + ');
+}
+else if($('.par_'+parent).hasClass("new_display2"))
+{
+  $('.par_'+parent).removeClass('new_display2');
+$('.par_'+parent).addClass('display_block');
+
+$('.param_'+parent).html(' - ');
+}
+
+ 
+ 
+    
+});
